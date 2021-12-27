b90e7cfd2176ef6f9a76a7eb983a7b49243d7321
ken0911208818 <u1045235@hk.edu.tw>
2021-12-27 11:31:00 +0800

    fix test case

--- leetcode/sortString/question.go
+++ leetcode/sortString/question.go
@@ -1,20 +1,17 @@
 package sortstring
 
 import (
-	"fmt"
 	"sort"
 )
 
 func sortString(input ...string) []string {
 	sort.Strings(input)
-	for _, v := range input {
-		fmt.Println([]byte(v[:1]))
-		fmt.Println(v[:1])
-		// if []byte(v[:1])[0] > byte(90) {
+	for i, v := range input {
+		if []byte(v[:1])[0] > byte(90) {
 
-		// 	result := append(input[i:], input[:i]...)
-		// 	return result
-		// }
+			result := append(input[i:], input[:i]...)
+			return result
+		}
 	}
 	return input
 }
